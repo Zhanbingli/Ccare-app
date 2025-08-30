7f19a0fb945f8003cfc7fc58eae7947ea7d31a16
zhanbingli <3163525375@qq.com>
2025-08-30 21:31:41 +0800

    Initial Commit

---
+++ ChronicCare/ChronicCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicCareApp.swift
+//  ChronicCare
+//
+//  Created by lizhanbing12 on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChronicCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChronicCareTests/ChronicCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicCareTests.swift
+//  ChronicCareTests
+//
+//  Created by lizhanbing12 on 30/08/25.
+//
+
+import Testing
+@testable import ChronicCare
+
+struct ChronicCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
